3e8cba088c8d04ee883fef93ea0175480d8f00d2
fmash16 <fmash16@gmail.com>
2020-06-09 08:55:25 +0600

    edits

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-chmod 755 mashbash.sh && cp mashbash.sh /usr/bin/
-chmod 755 revshell.sh && cp revshell.sh /usr/bin/
+chmod 755 mashbash.sh && cp mashbash.sh /usr/bin/mashbash
+chmod 755 revshell.sh && cp revshell.sh /usr/bin/mashbash
